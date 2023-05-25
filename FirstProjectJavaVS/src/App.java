1ec6fa36a56d1eee03931960465c51570e882871
Samuel Brito <sambriton7@gmail.com>
2023-05-25 18:37:42 -0300

    First code using Java

---
+++ FirstProjectJavaVS/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        int a = 12;
+        int b = 15;
+        int soma = a + b;
+        float divisão = (float) a / b;
+
+        System.out.println(soma);
+        System.out.println(divisão);
+    }
+}
