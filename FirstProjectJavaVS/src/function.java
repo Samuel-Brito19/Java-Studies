ae0392348cc596ab64db721a8674dadb0f8f89fe
Samuel Brito <sambriton7@gmail.com>
2023-05-26 10:22:05 -0300

    Functions and arrays

---
+++ FirstProjectJavaVS/src/function.java
@@ -0,0 +1,13 @@
+public class function {
+
+    public static void main(String[] args) throws Exception {
+
+        String nameOriginal = "Vini Jr";
+        saudacao(nameOriginal);
+
+    }
+
+    public static void saudacao(String namParameter) {
+        System.out.println("Hello, " + namParameter);
+    }
+}
---
+++ FirstProjectJavaVS/src/vetores.java
@@ -0,0 +1,11 @@
+public class vetores {
+
+    public static void main(String[] args) throws Exception {
+
+        String[] letras = { "A", "B", "C", "D" };
+        for (int i = 0; i < letras.length; i++) {
+            System.out.println(letras[i]);
+        }
+
+    }
+}
